a7c50ae04b20127adfc36869061fcd33d3201573
bhargavtelapudi <62876599+bhargav443@users.noreply.github.com>
2022-09-27 19:39:35 -0500

    inital commit

--- Assignment1/src/com/Automate/Employe.java
+++ Assignment1/src/com/Automate/Employe.java
@@ -1,2 +1,19 @@
-package com.Automate;public class Employe {
+package com.Automate;
+/* employee The pojo which defines the properties and behaviour of the employee*/
+public class Employe {
+    private String Name;
+    private int age;
+    private double salary;
+    private int uniqueIdentifer;
+
+    public Employe(String name, int age, double salary, int uniqueIdentifer) {
+
+        this.Name = name;
+        this.age = age;
+        this.salary = salary;
+        this.uniqueIdentifer = uniqueIdentifer;
+    }
+    public void addemploye(){
+    System.out.println(this.age+" "+this.salary+" "+this.Name+" "+this.uniqueIdentifer);
+}
 }
