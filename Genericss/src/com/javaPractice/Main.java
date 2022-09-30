96f9c0dd4b8c49207c46079e9313c271d8f00fce
bhargavtelapudi <62876599+bhargav443@users.noreply.github.com>
2022-09-30 12:17:23 -0500

    complete code

--- Genericss/src/com/javaPractice/Main.java
+++ Genericss/src/com/javaPractice/Main.java
@@ -5,19 +5,42 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        ArrayList<Integer> items = new ArrayList<>();
-        items.add(1);
-        items.add(2);
-        items.add(3);
-        items.add(4);
+        // write your code here
+        FootballPlayer joe =new FootballPlayer("joe");
+        BaseballPlayer pat =new BaseballPlayer("pat");
+        SoccerPlayer bechkam =new SoccerPlayer("sue");
 
-        printDoubled(items);
+         Team<FootballPlayer> adelaideCrows = new Team<>("adelaide Crows");
+         adelaideCrows.addPlayer(joe);
+        System.out.println(adelaideCrows.numberOfPlyers());
 
-    }
-    private static void printDoubled(ArrayList<Integer>  n){
-        for (Integer i : n){
-            System.out.println(i*3);
-        }
+        Team<BaseballPlayer> adelaideeCrows = new Team<>("chicago Crows");
+        adelaideeCrows.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this wont work");
+        brokenTeam.addPlayer(bechkam);
+
+        Team<FootballPlayer> melnorne =new Team<>("meloborne");
+        FootballPlayer banks =new FootballPlayer("goardon");
+        melnorne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn = new Team<>("hawthorn");
+        Team<FootballPlayer> fremantel = new Team<>("fremantel");
+        hawthorn.matchResult(fremantel,1,0);
+        hawthorn.matchResult(adelaideCrows,3,9);
+        adelaideCrows.matchResult(fremantel,2,1);
+
+        System.out.println("Rankings");
+        System.out.println(adelaideCrows.getName()+""+adelaideCrows.ranking());
+        System.out.println(melnorne.getName()+""+melnorne.ranking());
+        System.out.println(fremantel.getName()+""+fremantel.ranking());
+        System.out.println(hawthorn.getName()+""+hawthorn.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melnorne));
+        System.out.println(adelaideCrows.compareTo(hawthorn));
+        System.out.println(hawthorn.compareTo(adelaideCrows));
+        System.out.println(melnorne.compareTo(fremantel));
+
+
     }
 }
